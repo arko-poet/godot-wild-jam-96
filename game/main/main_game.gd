ace9dd27694162eb39743c8be3f8092fbc9b0171
arko <arkadiusz.kupracz@gmail.com>
2026-08-15 15:35:15 +0100

    handle game losing

--- game/main/main_game.gd
+++ game/main/main_game.gd
@@ -13,16 +13,18 @@ var _ectoplasm: int:
 		_ectoplasm_label.text = "Ectoplasm: %s" % _ectoplasm
 var _core_charges: int:
 	set(value):
-		_core_charges = value
+		_core_charges = max(value, 0)
 		_core_charges_label.text = "Core Charges: %s" % _core_charges
+		
+		if _core_charges == 0:
+			_win_lose_manager.game_lost()
 
-@onready var _level: Level = %Level
+@onready var _pause_menu_controller: Node = %PauseMenuController
+@onready var _win_lose_manager: Node = %WinLoseManager
 
 @onready var _core_charges_label: Label = %CoreChargesLabel
 @onready var _ectoplasm_label: Label = %EctoplasmLabel
 @onready var _wave_label: Label = %WaveLabel
-
-@onready var _pause_menu_controller: Node = %PauseMenuController
 
 
 func _ready() -> void:
@@ -40,7 +42,8 @@ func _on_place_tower_button_pressed() -> void:
 
 
 func _on_start_wave_button_pressed() -> void:
-	_wave += 1
+	# TODO handle wave starting and counter updates here
+	pass # Replace with function body.
 
 
 func _on_pause_button_pressed() -> void:
