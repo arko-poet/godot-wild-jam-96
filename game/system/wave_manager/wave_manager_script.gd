d6b1c65070ec99f62cd73626e942746479631540
arko <arkadiusz.kupracz@gmail.com>
2026-08-19 10:17:48 +0100

    fix wave count updates

--- game/system/wave_manager/wave_manager_script.gd
+++ game/system/wave_manager/wave_manager_script.gd
@@ -3,14 +3,18 @@ class_name WaveManager extends Node
 signal wave_limit_exceeded
 
 @onready var _wave_label: Label = %WaveLabel
-var _current_wave: int = 0
+var _current_wave: int = 0:
+	set(value):
+		_current_wave = value
+		_wave_label.text =  "%s/%s" % [_current_wave, level.get_number_of_waves()]
+		
 var _active_wave: bool = false
 
 @onready var start_wave_button: Button = $"../UILayer/UI/StartWaveButton"
 @onready var level: Level = %Level
 
 func _ready() -> void:
-	_wave_label.text =  "%s/100" % _current_wave
+	_current_wave = _current_wave
 	start_wave_button.pressed.connect(_on_start_wave_button)
 	Event.wave_ended_signal.connect(_on_wave_ended_signal)
 
@@ -26,7 +30,10 @@ func _on_wave_ended_signal()->void:
 
 func _next_wave()->void:
 	_current_wave += 1
-	_wave_label.text =  "%s/100" % _current_wave
+	
+	if _current_wave > level.get_number_of_waves():
+		wave_limit_exceeded.emit()
+	
 	Event.next_wave_trigered(_current_wave)
 
 
