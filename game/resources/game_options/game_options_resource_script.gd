e079420c2b02e9d34c128feccb80530573e1e625
Jerrylikesgreen <jerrylikesgreen@gmail.com>
2026-08-22 16:54:19 -0400

    Wire tips toggle UI to GameOptionsResource
    Add a setter to GameOptionsResource and connect the ToogleTips control to the resource. The scene now references game_options_resource.tres; the script initializes the CheckButton with set_pressed(...) and updates the resource via set_tips_toogled(...) when toggled. This fixes prior property/method usage and ensures tip state is stored through the resource.

--- game/resources/game_options/game_options_resource_script.gd
+++ game/resources/game_options/game_options_resource_script.gd
@@ -2,3 +2,7 @@ class_name GameOptionsResource extends Resource
 
 
 var is_tips_toogled: bool = true
+
+
+func set_tips_toogled( toogled: bool  )->void:
+	is_tips_toogled = toogled
--- template/scenes/menus/options_menu/game/toogle_tips_control/toogle_tips_script.gd
+++ template/scenes/menus/options_menu/game/toogle_tips_control/toogle_tips_script.gd
@@ -6,9 +6,9 @@ extends HBoxContainer
 func _ready() -> void:
 	check_button.toggled.connect(_on_tips_toggled)
 	
-	check_button.button_pressed = game_options_resource.is_tips_toogle
+	check_button.set_pressed(game_options_resource.is_tips_toogled)
 
 
 func _on_tips_toggled(is_toggled: bool) -> void:
 	Event.tips_toogled(is_toggled)
-	game_options_resource.is_tips_toogled = is_toggled
+	game_options_resource.set_tips_toogled(is_toggled)
